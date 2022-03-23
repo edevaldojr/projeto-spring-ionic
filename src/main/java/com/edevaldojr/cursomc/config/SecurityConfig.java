0e8b823a46c3cdd30de1a0d772a72d2436067c06
Edevaldo Junior <ed.tadsifpr@gmail.com>
2022-03-23 16:18:43 -0300

    Implementando autorizacao

--- src/main/java/com/edevaldojr/cursomc/config/SecurityConfig.java
+++ src/main/java/com/edevaldojr/cursomc/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.edevaldojr.cursomc.config;
 import java.util.Arrays;
 
 import com.edevaldojr.cursomc.security.JWTAuthenticationFilter;
+import com.edevaldojr.cursomc.security.JWTAuthorizationFilter;
 import com.edevaldojr.cursomc.security.JWTUtil;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -57,6 +58,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(PUBLIC_MATCHERS).permitAll()
                 .anyRequest().authenticated();
         http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+        http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
