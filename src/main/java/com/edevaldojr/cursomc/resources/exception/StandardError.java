e613a47a531aab3d47c496360add2bf0e8ed0944
Edevaldo Junior <ed.tadsifpr@gmail.com>
2022-04-05 17:39:26 -0300

    Padronizando o formato de excecoes

--- src/main/java/com/edevaldojr/cursomc/resources/exception/StandardError.java
+++ src/main/java/com/edevaldojr/cursomc/resources/exception/StandardError.java
@@ -2,35 +2,60 @@ package com.edevaldojr.cursomc.resources.exception;
 
 import java.io.Serializable;
 
-public class StandardError implements Serializable{
-    
-    private Integer status;
-    private String msg;
+public class StandardError implements Serializable {
+
     private Long timeStamp;
-    
-    public StandardError(Integer status, String msg, Long timeStamp) {
-        super();
-        this.status = status;
-        this.msg = msg;
+    private Integer status;
+    private String error;
+    private String message;
+    private String path;
+
+    public StandardError(Long timeStamp, Integer status, String error, String message, String path) {
         this.timeStamp = timeStamp;
-    }
-    public Integer getStatus() {
-        return status;
-    }
-    public void setStatus(Integer status) {
         this.status = status;
+        this.error = error;
+        this.message = message;
+        this.path = path;
     }
-    public String getMsg() {
-        return msg;
-    }
-    public void setMsg(String msg) {
-        this.msg = msg;
-    }
+
     public Long getTimeStamp() {
         return timeStamp;
     }
+
     public void setTimeStamp(Long timeStamp) {
         this.timeStamp = timeStamp;
     }
-    
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
 }
